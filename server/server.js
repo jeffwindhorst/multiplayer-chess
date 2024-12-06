9a449b833adb083d6cc2c26e14d89bff1faf6627
Jeff Windhorst <jeffwindhorst@gmail.com>
2024-12-06 14:44:04 -0500

    Fixed the room players count

--- server/server.js
+++ server/server.js
@@ -1,73 +1,78 @@
-const express = require('express');
+const express = require("express");
 const { Server } = require("socket.io");
-const { v4: uuidV4 } = require('uuid');
-const http = require('http');
+const { v4: uuidV4 } = require("uuid");
+const http = require("http");
 
 const app = express(); // initialize express
 
 const server = http.createServer(app);
 
-
 // set port to value received from environment variable or 8080 if null
-const port = process.env.PORT || 8080 
+const port = process.env.PORT || 8080;
 
 // upgrade http server to websocket server
 const io = new Server(server, {
-  cors: '*', // allow connection from any origin
+  cors: "*", // allow connection from any origin
 });
 
 const rooms = new Map();
 
 // io.connection
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   // socket refers to the client socket that just got connected.
   // each socket is assigned an id
-  console.log(socket.id, 'connected');
+  console.log(socket.id, "connected");
 
-  socket.on('username', (username) => {
-    console.log('username:', username);
+  socket.on("username", (username) => {
+    console.log("username:", username);
     socket.data.username = username;
   });
 
-  socket.on('createRoom', async (callback) => { // callback here refers to the callback function from the client passed as data
+  socket.on("createRoom", async (callback) => {
+    // callback here refers to the callback function from the client passed as data
     const roomId = uuidV4(); // <- 1 create a new uuid
     await socket.join(roomId); // <- 2 make creating user join the room
-	 
+
     // set roomId as a key and roomData including players as value in the map
-    rooms.set(roomId, { // <- 3
+    rooms.set(roomId, {
+      // <- 3
       roomId,
-      players: [{ id: socket.id, username: socket.data?.username }]
+      players: [{ id: socket.id, username: socket.data?.username }],
     });
     // returns Map(1){'2b5b51a9-707b-42d6-9da8-dc19f863c0d0' => [{id: 'socketid', username: 'username1'}]}
 
     callback(roomId); // <- 4 respond with roomId to client by calling the callback function from the client
   });
 
-  socket.on('joinRoom', async (args, callback) => {
+  socket.on("joinRoom", async (args, callback) => {
     // check if room exists and has a player waiting
     const room = rooms.get(args.roomId);
     let error, message;
-  
-    if (!room) { // if room does not exist
+
+    if (!room) {
+      // if room does not exist
       error = true;
-      message = 'room does not exist';
-    } else if (room.length <= 0) { // if room is empty set appropriate message
+      message = "room does not exist";
+    } else if (room.players.length <= 0) {
+      // if room is empty set appropriate message
       error = true;
-      message = 'room is empty';
-    } else if (room.length >= 2) { // if room is full
+      message = "room is empty";
+    } else if (room.players.length >= 2) {
+      // if room is full
       error = true;
-      message = 'room is full'; // set message to 'room is full'
+      message = "room is full"; // set message to 'room is full'
     }
 
     if (error) {
       // if there's an error, check if the client passed a callback,
-      // call the callback (if it exists) with an error object and exit or 
+      // call the callback (if it exists) with an error object and exit or
       // just exit if the callback is not given
 
-      if (callback) { // if user passed a callback, call it with an error payload
+      if (callback) {
+        // if user passed a callback, call it with an error payload
         callback({
           error,
-          message
+          message,
         });
       }
 
@@ -90,18 +95,19 @@ io.on('connection', (socket) => {
     callback(roomUpdate); // respond to the client with the room details.
 
     // emit an 'opponentJoined' event to the room to tell the other player that an opponent has joined
-    socket.to(args.roomId).emit('opponentJoined', roomUpdate);
+    socket.to(args.roomId).emit("opponentJoined", roomUpdate);
   });
 
-  socket.on('move', (data) => {
+  socket.on("move", (data) => {
     // emit to all sockets in the room except the emitting socket.
-    socket.to(data.room).emit('move', data.move);
+    socket.to(data.room).emit("move", data.move);
   });
 
   socket.on("disconnect", () => {
     const gameRooms = Array.from(rooms.values()); // <- 1
 
-    gameRooms.forEach((room) => { // <- 2
+    gameRooms.forEach((room) => {
+      // <- 2
       const userInRoom = room.players.find((player) => player.id === socket.id); // <- 3
 
       if (userInRoom) {
